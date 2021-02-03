98d4b3c0f58249672618bc4ba423783072a63438
yvt <i@yvt.jp>
2021-02-03 13:17:00 +0900

    feat(bin): implement the basic CLI behavior

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Clap;
+use std::path::PathBuf;
+
+#[derive(Clap, Debug)]
+struct Opts {
+    /// The image to process
+    pub image: PathBuf,
+}
+
 fn main() {
-    println!("saluton");
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("img2text=info"))
+        .init();
+
+    let opts: Opts = Clap::parse();
+    log::debug!("opts = {:#?}", opts);
+
+    log::info!("saluton");
 }
