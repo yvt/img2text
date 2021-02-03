95b067fd6e8723b008eb86064b4c3714833b0a65
yvt <i@yvt.jp>
2021-02-03 13:23:12 +0900

    feat(bin): load a given image

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,19 @@ use std::path::PathBuf;
 #[derive(Clap, Debug)]
 struct Opts {
     /// The image to process
-    pub image: PathBuf,
+    pub image_path: PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("img2text=info"))
         .init();
 
     let opts: Opts = Clap::parse();
     log::debug!("opts = {:#?}", opts);
 
+    let _img = image::open(&opts.image_path)?;
+
     log::info!("saluton");
+
+    Ok(())
 }
